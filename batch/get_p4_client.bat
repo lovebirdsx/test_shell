4ed7f80ba2cff592574e2549a8e18c9d28f5323c
lovebird <lovebirdsx@gmail.com>
2023-03-20 14:12:30 +0800

    fix bug

--- batch/get_p4_client.bat
+++ batch/get_p4_client.bat
@@ -3,7 +3,10 @@ setlocal EnableDelayedExpansion
 
 set CWD=%cd%
 for /f "tokens=2,5 delims= " %%a in ('p4 clients --me') do (
-	echo !CWD! | findstr /i /c:"%%b:/=\" > nul && (
+	set ROOT=%%b
+	@REM 统一路径分隔符, 以便于findstr中路径统一匹配
+	set ROOT=!ROOT:/=\!
+	echo !CWD! | findstr /i /c:"!ROOT!" > nul && (
 		echo %%a
 		exit /b 0
 	)
